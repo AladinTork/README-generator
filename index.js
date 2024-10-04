36214e17db29885aba9ae68a22c214ecb59d43f2
Arman Tork <canadienperse@gmail.com>
2024-10-03 18:31:08 -0700

    starter code

---
+++ index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is the title?',
+    'What is the description?',
+    'What is the installation instructions?',
+    'What are the usage informations?',
+    'What are the contribution guidelines?',
+    ''
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
